5d9eb0bcc770fb39dd9d852e7741e897e597a363
Dipjoy Basak <62418148+DIPJOY10@users.noreply.github.com>
2022-11-10 20:56:53 +0530

    Update PostOrder.cpp

--- PostOrder.cpp
+++ PostOrder.cpp
@@ -16,3 +16,33 @@ public:
         return ans;
     }
 };
+
+//iterative
+class Solution {
+public:
+
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        if(!root){
+            return ans;
+        }
+        stack<TreeNode*> st1,st2;
+        st1.push(root);
+        while(!st1.empty()){
+            auto it=st1.top();
+            st1.pop();
+            st2.push(it);
+            if(it->left){
+                st1.push(it->left);
+            }
+            if(it->right){
+                st1.push(it->right);
+            }
+        }
+        while(!st2.empty()){
+            ans.push_back(st2.top()->val);
+            st2.pop();
+        }
+        return ans;
+    }
+};
