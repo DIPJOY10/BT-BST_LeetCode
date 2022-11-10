385169cd4fbd5f4ed8399012581d9f5e7ca2ac7c
Dipjoy Basak <62418148+DIPJOY10@users.noreply.github.com>
2022-11-10 23:48:03 +0530

    Create Height.cpp

---
+++ Height.cpp
@@ -0,0 +1,11 @@
+//Height of binary tree/Max Depth 
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(!root){
+            return 0;
+        }
+        return 1+max(maxDepth(root->left),maxDepth(root->right));
+    }
+};
