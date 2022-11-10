fa965b205257cdf4efd9afefa14346de379a63d3
Dipjoy Basak <62418148+DIPJOY10@users.noreply.github.com>
2022-11-10 20:03:31 +0530

    Create PostOrder.cpp

---
+++ PostOrder.cpp
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/binary-tree-postorder-traversal/
+
+class Solution {
+public:
+    void postorder(TreeNode* root,vector<int>& ans){
+        if(!root)
+        return;
+        postorder(root->left,ans);
+        postorder(root->right,ans);
+        ans.push_back(root->val);
+    }
+
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        postorder(root,ans);
+        return ans;
+    }
+};
